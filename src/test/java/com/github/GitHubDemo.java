8e8d8a546e145747c0f9944ffdf56780f295c945
jazebnadeem <jazebnadeem217@gmail.com>
2016-12-11 14:23:51 -0500

    New Change

--- src/test/java/com/github/GitHubDemo.java
+++ src/test/java/com/github/GitHubDemo.java
@@ -3,5 +3,6 @@ package com.github;
 public class GitHubDemo {
 	public static void main(String[] args) {
 		System.out.println("GitHub Project Demo");
+		System.out.println("Another print message");
 	}
 }
